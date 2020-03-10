52b0a2f03374d36486e2f3ba1f8f63ae75882350
Maurice Falls <falls8019oct@gmail.com>
2020-03-10 03:12:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   answer="HUH?! SPEAK UP, SONNY!"
-  if phrase.uppercase==
+  if phrase.upcase==
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase=="I LOVE YOU GRANDMA"
   puts "I LOVE YOU TOO PUMPKIN!"
