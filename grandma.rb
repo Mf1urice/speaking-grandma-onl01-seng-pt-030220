0b2c7ac8fecedafe35da3cde2ff2d48c76d3a6d3
Maurice Falls <falls8019oct@gmail.com>
2020-03-10 03:27:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
   if phrase=="I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase== phrase.upcase
-  puts "I LOVE YOU TOO PUMPKIN!"
+  "NO, NOT SINCE 1938!"
 else phrase.uppercase
   puts "NO, NOT SINCE 1938!"
 end
